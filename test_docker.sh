f6b0452ad58717eb4acd782cf17d641da82842bf
pamelamishra@gmail.com <mishrap@reslnvhpc053.research.chop.edu>
2025-11-17 10:12:31 -0500

    New docker

--- test_docker.sh
+++ test_docker.sh
@@ -1,3 +1,3 @@
-singularity exec docker://ghcr.io/chop-cccr/rna-cccr-pipeline:v0.3.0 \
+singularity exec docker://ghcr.io/pamelanmrc/rna-seq-nextflow-docker:latest \
   bash -lc 'which STAR; STAR --version; which fastqc; which samtools'
 
